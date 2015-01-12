134462c82894f6427d717863373a9931e5383140
ruidge <ruidge@gmail.com>
2015-01-12 22:07:15 +0800

    update

--- Test/com/ruidge/algorithm/mergesort.py
+++ Test/com/ruidge/algorithm/mergesort.py
@@ -1,25 +1,11 @@
-# coding=utf-8
+#coding=utf-8
 '''
-Created on 2015年1月10日
+Created on 2015年1月12日
 
 @author: ruidge
 '''
 from types import NoneType
 data = [ 20, 3, 2, 1, 40, 23, 54, 5, 56, 7, 9, 89, 10, 7, 6 ]
-def insertSort(data):
-    if(data is None or len(data) == 0 or len(data) == 1):
-        return
-    else:
-        for i in range(1, len(data)):
-            j = i - 1
-            key = data[i]
-            while(j >= 0):
-                if(data[j] > key):
-                    data[j + 1] = data[j]
-                    j = j - 1
-                else:
-                    break
-            data[j + 1] = key
 
 def mergeSort(data):
     if(type(data) is NoneType or len(data) == 0):
@@ -53,9 +39,6 @@ def mergeData(temp1, temp2):
     return result
         
 if __name__ == '__main__':
-    cloneData1 = data[:]
-    print cloneData1
-    insertSort(cloneData1)
-    print cloneData1
+    print data
     cloneData2 = data[:]
     print mergeSort(cloneData2)
