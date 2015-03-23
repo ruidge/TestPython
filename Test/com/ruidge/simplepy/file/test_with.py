b369593d148271e5a46b8701e891c26a2973beb9
ruidge@gmail.com <ruidge@gmail.com>
2015-03-23 14:04:27 +0800

    0323 sync

--- Test/com/ruidge/simplepy/file/test_with.py
+++ Test/com/ruidge/simplepy/file/test_with.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     with get_sample() as sample:
         time.sleep(1)
 #        写这里异常后__exit__还会走到并传过去,但是print没有走到
-        e = 1 / 0
+#         e = 1 / 0
         print "sample:", sample
 
 # 紧跟with后面的语句被求值后，返回对象的__enter__()方法被调用，这个方法的返回值将被赋值给as后面的变量。
